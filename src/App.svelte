81cc2cda93f5224de784854703c4bd89851f8c58
shawn edgell <shawn_edgell@hotmail.com>
2023-10-21 16:43:56 -0400

    added logo as module

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import PracticalExp from './components/PracticalExp.svelte';
   import Skills from './components/Skills.svelte';
   import Certificates from './components/Certificates.svelte';
+  import logo from './assets/cv-logo.svg';
 </script>
 
 <style>
@@ -11,7 +12,7 @@
 </style>
 
 <main>
-  <img id="logo" src="./public/cv-logo.svg" alt="CV Creator Logo">
+  <img id="logo" src={logo} alt="CV Creator Logo">
   <h1>CV Creator</h1>
   
   <div class="inputs-container">
