e1164963ba296e3ad01d498bfdfedf162452074c
shawn edgell <shawn_edgell@hotmail.com>
2023-10-22 04:51:35 -0400

    added some CV styling

--- src/components/CVDisplay.svelte
+++ src/components/CVDisplay.svelte
@@ -6,6 +6,7 @@
   * {
     padding: 0;
     margin: 0;
+    font-family:'Times New Roman', Times, serif;
   }
 
   .cv-container {
@@ -33,13 +34,37 @@
   #title {
     font-size: 3.2em;
     font-weight: 600;
+    text-align: center;
   }
 
+  h2 {
+    background-color: #e8eaf3;
+  }
+
+  .cv-container-body {
+    padding: 40px 60px;
+  }
+
+  .skills-section {
+    display: flex;
+    gap: 20px;
+    padding: 12px 0;
+    justify-content: center;
+  }
+
+  p, li {
+    text-align: left;
+  }
+
+  .text-box {
+    padding: 12px 0;
+  }
 
 </style>
 
 <div class="cv-container">
   <!-- General Information -->
+
   <div class="general-info">
     <p id="title">{$generalInfo.name}</p>
     <div class="contact-info">
@@ -48,15 +73,19 @@
     </div>
   </div>
 
+  <div class="cv-container-body">
+
   <!-- Education -->
   {#if $educations.length > 0}
   <div class="education-section">
     <h2>Education</h2>
+    <div class="text-box">
     <ul>
       {#each $educations as education}
       <li>{education.schoolName}, {education.titleOfStudy}, {education.dateOfStudy}</li>
       {/each}
     </ul>
+  </div>
   </div>
   {/if}
 
@@ -64,7 +93,8 @@
 {#if $experiences.length > 0}
 <div class="experience-section">
   <h2>Practical Experience</h2>
-  <ul>
+  <div class="text-box">
+    <ul>
     {#each $experiences as experience}
     <li>
       <p><strong>{experience.companyName}</strong></p>
@@ -75,18 +105,17 @@
     {/each}
   </ul>
 </div>
+</div>
 {/if}
 
 
   <!-- Skills -->
   {#if $skills.length > 0}
+  <h2>Skills</h2>
   <div class="skills-section">
-    <h2>Skills</h2>
-    <ul>
       {#each $skills as skill}
       <li>{skill}</li>
       {/each}
-    </ul>
   </div>
   {/if}
 
@@ -94,11 +123,14 @@
   {#if $certificates.length > 0}
   <div class="certificates-section">
     <h2>Certificates</h2>
+    <div class="text-box">
     <ul>
       {#each $certificates as certificate}
       <li>{certificate.name} from {certificate.institution} on {certificate.date}</li>
       {/each}
     </ul>
   </div>
+  </div>
   {/if}
+</div>
 </div>
